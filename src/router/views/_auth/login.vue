5c1ccf458304a8daa3055e342e8973ba9f4330b5
AccountNo00 <itsmejohnnnnn@gmail.com>
2024-03-27 10:50:26 +0800

    login design update

--- src/router/views/_auth/login.vue
+++ src/router/views/_auth/login.vue
@@ -41,32 +41,31 @@ export default {
 
 <template>
 	<Layout>
+		<img class="wave" src="../../../assets/images/wave.png">
 		<div class="row justify-content-center">
-			<div class="main-container mt-5">
-				<div class="col-2 mx-auto">
-					<div class="row">
-						<label>
-							Email
-							<input
-								class="form-control mb-2"
-								placeholder="Enter email"
-								type="text"
-								v-model="username"
-								v-on:keyup.enter="login()"
-							/>
-						</label>
-						<label>
-							Password
-							<input
-								class="form-control mb-2"
-								placeholder="Enter password"
-								type="password"
-								v-model="password"
-								v-on:keyup.enter="login()"
-							/>
-						</label>
+			<div class="login-container">
+				<img class="logo" src="../../../assets/images/crm-ss.png">
+				<h2 class="title">WELCOME</h2>
+				<div class="col-4 mx-auto">
+					<div class="row inputs">
+						<i class="fas fa-user"></i>
+						<input
+							class="form-control mb-2 border-red"
+							placeholder="Username"
+							type="text"
+							v-model="username"
+							v-on:keyup.enter="login()"
+						/>
+						<i class="fas fa-lock"></i>
+						<input
+							class="form-control mb-2 border-red"
+							placeholder="Password"
+							type="password"
+							v-model="password"
+							v-on:keyup.enter="login()"
+						/>
 					</div>
-					<button class="btn btn-info w-100" @click="login()">
+					<button class="btn btn-info w-100 login-btn" @click="login()">
 						Login
 					</button>
 				</div>
@@ -74,3 +73,63 @@ export default {
 		</div>
 	</Layout>
 </template>
+<style scoped>
+.login-btn{
+	display: block;
+    width: 100%;
+    height: 50px;
+    border-radius: 25px;
+    outline: none;
+    border: none;
+    background-color: red;
+    opacity: 0.7;
+    background-size: 200%;
+    font-size: 1.2rem;
+    color: #fff;
+    font-family: 'Poppins', sans-serif;
+    text-transform: uppercase;
+    margin: 2rem 0;
+    padding-top: 10px;
+    cursor: pointer;
+    transition: .5s;
+}
+.inputs > i{
+	position: relative;
+	top: 38px;
+	font-size: 22px;
+}
+.border-red{
+    font-size: 25px !important;
+	border:none;
+    outline: none;
+    background: none;
+	border-radius: 0;
+    padding: 0.5rem 0.7rem;
+	padding-left: 50px;
+    font-size: 1.2rem;
+    color: #555 !important;
+    font-family: 'poppins', sans-serif;
+    border-bottom: 3px solid red;
+}
+.title {
+    margin: 15px 0;
+    color: #333;
+    text-transform: uppercase;
+    font-size: 2.9rem;
+}
+.login-container{
+	margin-top: 150px;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+}
+.logo{
+	height: 250px;
+}
+.wave{
+	position: absolute;
+	left: 0;
+	top: 0;
+	height: 100%;
+}
+</style>
